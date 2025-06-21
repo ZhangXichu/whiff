38368b7a81357dfd84bce868833715d57e4b7ccc
ZhangXichu <xichu.zhang@yahoo.com>
2025-06-21 17:49:18 +0200

    refactor: move data structs to separate file

--- src/handshake_extractor.hpp
+++ src/handshake_extractor.hpp
@@ -1,55 +1,13 @@
 #pragma once
 
 #include <pcap.h>
-#include <vector>
-#include <cstdint>
 #include <string>
-#include <array>
 #include <optional>
+#include <packet_data.hpp>
 
 namespace whiff {
 
-struct EapolPacket 
-{
-    struct timeval timestamp;
-    std::vector<uint8_t> raw_data;
-};
 
-
-struct KeyDescriptor {
-    uint8_t descriptor_type;
-    uint16_t key_info;
-    uint16_t key_length;
-    std::array<uint8_t, 8> replay_counter;
-    std::array<uint8_t, 32> nonce;
-    std::array<uint8_t, 16> key_iv;
-    std::array<uint8_t, 8> key_rsc;
-    std::array<uint8_t, 16> mic;
-    uint16_t key_data_length;
-    std::vector<uint8_t> key_data;
-};
-
-
-struct Eapol {
-    timeval timestamp;
-    std::vector<uint8_t> raw_frame;
-    std::array<uint8_t, 6> src_mac{};  // Source MAC address
-    std::array<uint8_t, 6> dst_mac{};  // Destination MAC address
-    bool has_mic = false;
-    bool is_from_ap = false;
-    KeyDescriptor key_descriptor;
-};
-
-
-struct HandshakeData {
-    std::array<uint8_t, 6> ap_mac;
-    std::array<uint8_t, 6> client_mac;
-    std::array<uint8_t, 32> anonce;
-    std::array<uint8_t, 32> snonce;
-    std::array<uint8_t, 16> mic;
-    std::vector<uint8_t> eapol_frame;
-    std::string ssid;
-};
 
 class HandshakeExtractor {
 
---
+++ src/packet_data.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <array>
+#include <vector>
+#include <string>
+#include <cstdint>
+#include <sys/time.h>
+
+namespace whiff {
+
+struct EapolPacket 
+{
+    struct timeval timestamp;
+    std::vector<uint8_t> raw_data;
+};
+
+
+struct KeyDescriptor {
+    uint8_t descriptor_type;
+    uint16_t key_info;
+    uint16_t key_length;
+    std::array<uint8_t, 8> replay_counter;
+    std::array<uint8_t, 32> nonce;
+    std::array<uint8_t, 16> key_iv;
+    std::array<uint8_t, 8> key_rsc;
+    std::array<uint8_t, 16> mic;
+    uint16_t key_data_length;
+    std::vector<uint8_t> key_data;
+};
+
+
+struct Eapol {
+    timeval timestamp;
+    std::vector<uint8_t> raw_frame;
+    std::array<uint8_t, 6> src_mac{};  // Source MAC address
+    std::array<uint8_t, 6> dst_mac{};  // Destination MAC address
+    bool has_mic = false;
+    bool is_from_ap = false;
+    KeyDescriptor key_descriptor;
+};
+
+
+struct HandshakeData {
+    std::array<uint8_t, 6> ap_mac;
+    std::array<uint8_t, 6> client_mac;
+    std::array<uint8_t, 32> anonce;
+    std::array<uint8_t, 32> snonce;
+    std::array<uint8_t, 16> mic;
+    std::vector<uint8_t> eapol_frame;
+    std::string ssid;
+};
+
+}

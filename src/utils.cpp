705b4bbb934c0ab68fbc128d1171bedd72009586
ZhangXichu <xichu.zhang@yahoo.com>
2025-06-28 17:10:27 +0200

    add mac_from_bytes and mac_to_string

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,7 @@
 #include <sstream>
 #include <iomanip>
+#include <cstring>
+#include <array>
 #include <utils.hpp>
 
 namespace whiff {
@@ -11,6 +13,23 @@ std::string to_hex(const uint8_t* data, size_t len) {
         oss << std::hex << std::setfill('0') << std::setw(2) << (int)data[i];
     return oss.str();
 }
+
+std::array<uint8_t, 6> mac_from_bytes(const uint8_t* p) {
+    std::array<uint8_t, 6> mac;
+    std::memcpy(mac.data(), p, 6);
+    return mac;
+}
+
+std::string mac_to_string(const std::array<uint8_t, 6>& mac) {
+        std::ostringstream oss;
+        oss << std::hex << std::setfill('0');
+        for (size_t i = 0; i < mac.size(); ++i) {
+            oss << std::setw(2) << static_cast<int>(mac[i]);
+            if (i != mac.size() - 1)
+                oss << ":";
+        }
+        return oss.str();
+    }
 
 }
 }
--- src/utils.hpp
+++ src/utils.hpp
@@ -8,6 +8,8 @@ namespace whiff {
 namespace utils {
 
 std::string to_hex(const uint8_t* data, size_t len);
+std::array<uint8_t, 6> mac_from_bytes(const uint8_t* p);
+std::string mac_to_string(const std::array<uint8_t, 6>& mac);
 
 }
 }

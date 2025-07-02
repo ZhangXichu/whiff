5c8a4a1949417781da32f7362dee88e9e9deaaf4
ZhangXichu <xichu.zhang@yahoo.com>
2025-07-03 00:37:59 +0200

    add EapolFilter

---
+++ src/eapol_filter.hpp
@@ -0,0 +1,14 @@
+#include <packet_filter.hpp>
+
+
+namespace whiff {
+
+class EapolFilter : public PacketFilter {
+
+public:
+bool match(const u_char* packet, uint32_t len) const override;
+
+};
+
+
+}
